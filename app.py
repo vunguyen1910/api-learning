4097292a0325ee4c71a28fa53bbd36be9d8a8aa7
Viktor_nguyen <melodycenter19@gmail.com>
2019-12-11 15:49:03 +0700

    fix env

--- app.py
+++ app.py
@@ -5,7 +5,4 @@ from src import app
 
 if __name__ == "__main__":
     app.run(debug=True, ssl_context="adhoc")
-# 
-# video(order = '1', type ='video')
-# document = (order = '1a', type = 'doc')
-# result = Resource.query.filter_by(like(order = 1)).filter_by(type="doc").all()
+
