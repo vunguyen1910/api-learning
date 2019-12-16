c4e5460bb19b16f1b03e906f00c37ae6e55d74c5
Viktor_nguyen <melodycenter19@gmail.com>
2019-12-17 00:16:20 +0700

    testing

--- src/components/recourse/__init__.py
+++ src/components/recourse/__init__.py
@@ -15,6 +15,7 @@ def get_recourse(id):
 @recourse_blueprint.route('/singel-recourse/<id>')
 def get_singel_course(id):
     recourse_singel = Recourse.query.filter_by(course_id=id).first()
+    print(recourse_singel.id,"recourse singel here")
     return jsonify(data = recourse_singel.render())
 
 @recourse_blueprint.route('/create', methods=['POST'])
